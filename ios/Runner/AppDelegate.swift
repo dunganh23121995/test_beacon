c46301a7f2e3d6122db4cc9962d0943e5eb26067
Cao Anh Dung <dungca2312@gmail.com>
2022-01-27 16:17:42 +0700

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,10 +7,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      if (@available(iOS 10.0, *)) {
+        [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
+      }
     GeneratedPluginRegistrant.register(with: self)
-    if (@available(iOS 10.0, *)) {
-      [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
-    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
